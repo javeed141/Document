2839e66ddea903aec56b082734b3c443b849754b
javeed141 <javeedshaik7346@gmail.com>
2026-02-04 21:29:00 +0530

    changed url

--- fronted/src/api/api.tsx
+++ fronted/src/api/api.tsx
@@ -1,4 +1,4 @@
-export const api = import.meta.env.VITE_API_URL || "http://localhost:5000";
+export const api = import.meta.env.VITE_API_URL || "https://document-mi49.onrender.com";
 // api.interceptors.request.use((config) => {
 //   const token = localStorage.getItem("token");
 //   if (token) {
