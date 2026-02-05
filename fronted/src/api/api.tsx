dbce8c7c38504a40098f72102330e2327d47b0b4
javeed141 <javeedshaik7346@gmail.com>
2026-02-05 18:56:41 +0530

    hello

--- fronted/src/api/api.tsx
+++ fronted/src/api/api.tsx
@@ -1,7 +1,7 @@
-//  export const api="https://document-mi49.onrender.com"
+ export const api="https://document-mi49.onrender.com"
 
 
-export const api =  "http://localhost:5000";
+// export const api =  "http://localhost:5000";
 // api.interceptors.request.use((config) => {
 //   const token = localStorage.getItem("token");
 //   if (token) {
